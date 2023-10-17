762a227a89ab5e7231055e2965d87d2af3458a53
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2023-10-16 23:56:13 -0400

    use json to keep data while page reload

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import io
+import json
 import os
 import streamlit as st
 from dotenv import load_dotenv
+from collections import defaultdict
 
 import ocr_processor as op
 import image_helper as ih
 import langchain_helper as lch
 
-load_dotenv()
-openai_key = os.getenv("OPENAI_API_KEY")
+def save_data(data):
+    with open('prev_extracted_text.json', 'w') as file:
+        json.dump(data, file)
 
 if __name__ == "__main__":
+    load_dotenv()
+    openai_key = os.getenv("OPENAI_API_KEY")
+
+    data = {}
+    with open('prev_extracted_text.json', 'r') as file:
+        data = json.load(file)
+
     st.set_page_config(page_title="PDF Text Extractor")
 
     tab1, tab2 = st.tabs(["Text Extractor", "Translator"])
@@ -24,6 +34,8 @@ if __name__ == "__main__":
 
         fix = st.checkbox("Fix image orientation", value=True)
 
+        extract_btn_clicked = st.button(label="Extract Texts")
+
         if file is not None:
             # Pass the file as BytesIO stream
             pdf_data = io.BytesIO(file.read())
@@ -34,42 +46,52 @@ if __name__ == "__main__":
                 st.subheader(f"Page {i + 1}")
                 col1, col2 = st.columns(2)
                 col1.image(img, use_column_width=True)
-                text = op.extract_text_from_image(img, language=language)
-                col2.text(text)
+                if str(i) in data:
+                    col2.text_area("Text", data[str(i)], height=400)
+                else:
+                    col2.empty()
+                if extract_btn_clicked:
+                    text = op.extract_text_from_image(img, language=language)
+                    col2.text_area("Text", text, height=400)
+                    data[i] = text
+
+            save_data(data)
 
     with tab2:
         st.title("GPT-translator")
 
         if not openai_key:
-            st.text("Openai API key is required for this function.")
+            st.text("OpenAI API key is required for this function.")
         else:
             target_language = st.text_input(
-            label="Enter the language you want to translate into:",
-            max_chars=20,
-            value="English",
-        )
+                label="Enter the language you want to translate into:",
+                max_chars=20,
+                value="English",
+            )
 
-        source_text = st.text_area(
-            label="Enter source text here:",
-            max_chars=2000,
-            placeholder="Maxmium input charaters is 2000.",
-            height=300,
-        )
+            source_text = st.text_area(
+                label="Enter source text here:",
+                max_chars=2000,
+                placeholder="Maxmium input charaters is 2000.",
+                height=300,
+            )
 
-        clicked = st.button(label="Translate")
+            clicked = st.button(label="Translate")
+            loading = st.empty()
+            results = st.empty()
 
-        if clicked:
-            if source_text:
-                placeholder = st.text("Translating...")
-                response, cb = lch.translate_text(target_language, source_text).values()
-                st.write(response)
-                placeholder.empty()
-                st.divider()
-                st.caption(f"Request cost data:")
-                st.caption(f"input_char_count: {len(source_text)}")
-                st.caption(f"input_token_count: {cb.prompt_tokens}")
-                st.caption(f"output_char_count: {len(response)}")
-                st.caption(f"output_token_count: {cb.completion_tokens}")
-                st.caption(f"total_cost: ${cb.total_cost}")
-            else:
-                st.write("Input cannot be empty!")
+            if clicked:
+                if source_text:
+                    loading.text("Translating...")
+                    response, cb = lch.translate_text(target_language, source_text).values()
+                    results.write(response)
+                    loading.empty()
+                    st.divider()
+                    st.caption(f"Request cost data:")
+                    st.caption(f"input_char_count: {len(source_text)}")
+                    st.caption(f"input_token_count: {cb.prompt_tokens}")
+                    st.caption(f"output_char_count: {len(response)}")
+                    st.caption(f"output_token_count: {cb.completion_tokens}")
+                    st.caption(f"total_cost: ${cb.total_cost}")
+                else:
+                    loading.text("Input cannot be empty!")
--- run_app.bat
+++ run_app.bat
@@ -12,6 +12,9 @@ call venv\Scripts\activate
 rem Install packages from requirements.txt
 pip install -r requirements.txt
 
+rem Create an empty JSON object and overwrite the prev_extracted_text.json file
+echo {} > prev_extracted_text.json
+
 rem Run the main.py script
 streamlit run main.py --client.toolbarMode viewer
 
